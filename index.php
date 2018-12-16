218f3a42c2627fdd00cd396669ea4caae40aa312
Ken.su <suonkenzy@hotmail.com>
2018-12-16 18:44:24 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,322 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Kenzy Suon - CV</title>
+    <meta charset="utf-8"/>
+    <link href="styles.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css"
+          integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+<!-- Header -->
+<header id="header" class="alt">
+    <a href="#banner">Accueil</a></li>
+    <a href="#aboutme">A propos de moi</a></li>
+    <a href="#skills">Compétences</a>
+    <a href="#formations">Formations</a>
+    <a href="#exper">Expériences</a>
+</header>
+
+<!-- Banner -->
+<section id="banner">
+    <div class="inner">
+        <header>
+            <h1>Kenzy Suon</h1>
+            <p>Etudiant en 3ème année de Bachelor à l'ECE TECH<br/>
+                Spécialité Développement Mobile, Web
+                et Logiciel
+            </p>
+            <ul class="icons">
+                <li><a href="https://github.com/KennyKent/" title="Github" class="icon fa-github"><span
+                                class="label"></span></a></li>
+                <li><a href="https://www.linkedin.com/in/kenzy-suon-141076107/" title="Mon profil Linkedin"
+                       class="icon fa-linkedin"><span class="label"></span></a></li>
+                <li><a href="/cvWeb/images/CV.pdf" class="icon fa-download" download="CV"
+                       title="Download CV"></span></a>
+                </li>
+            </ul>
+        </header>
+    </div>
+</section>
+
+<main>
+    <!-- Section About Me -->
+    <section class="wrapper style1" id="aboutme">
+        <div class="inner">
+            <div class="flex flex-2">
+                <div class="col col1">
+                        <img src="images/0.jpg" alt=""/>
+                </div>
+                <div class="col col2">
+                    <h3>Kenzy Suon, 22 ans</h3>
+                    <h4>Etudiant en 3ème année de Bachelor à l'ECE TECH</h4>
+                    <p>Récemment diplomé d'un Brevet de Technicien Supérieur en services informatiques aux
+                        organisations, je
+                        poursuis actuellement mes études en Bachelor Spécialité Développement Mobile, Web et
+                        Logiciel à
+                        l'ECE TECH. Après l'obtention de mon diplôme, j'envisage de d'intégrer un Master.</p>
+                    <p>L'informatique a toujours été une de mes grandes passions. Je suis motivé et curieux, j'aime
+                        apprendre, expérimenter de nouvelles technologies et faire partager mes découvertes. </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Section Skills -->
+    <section class="wrapper style2" id="skills">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Compétences</h2>
+                </div>
+            </div>
+
+            <div class="col-md-12">
+
+                    <div class="progress-item">
+                        Développement Android - XML
+
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" aria-valuenow="65" aria-valuemin="0"
+                                 aria-valuemax="100" style="width: 65%"><span class="progress-percent"> 65%</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="progress-item">
+                        Développement Android - Java
+
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" aria-valuenow="35" aria-valuemin="0"
+                                 aria-valuemax="100" style="width: 35%"><span class="progress-percent"> 35%</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="progress-item">
+                        Java
+
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0"
+                                 aria-valuemax="100" style="width: 60%"><span class="progress-percent"> 60 %</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="progress-item">
+                        C#
+
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0"
+                                 aria-valuemax="100" style="width: 55%"><span class="progress-percent"> 55%</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="progress-item">
+                        Xamarin
+
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" aria-valuenow="30" aria-valuemin="0"
+                                 aria-valuemax="100" style="width: 30%"><span class="progress-percent"> 30%</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="progress-item">
+                        HTML/CSS
+
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0"
+                                 aria-valuemax="100" style="width: 55%;"><span class="progress-percent"> 55%</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="progress-item">
+                        PHP/MySQL
+
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0"
+                                 aria-valuemax="100" style="width: 25%;"><span class="progress-percent"> 25%</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="progress-item">
+                        Bootstrap
+
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" aria-valuenow="35" aria-valuemin="0"
+                                 aria-valuemax="100" style="width: 35%"><span class="progress-percent"> 35%</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="progress-item">
+                        Angular
+
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="0"
+                                 aria-valuemax="100" style="width: 15%;"><span class="progress-percent"> 15%</span>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Section Formations -->
+    <section class="wrapper style1" id="formations">
+        <div class="inner container-fluid">
+
+            <div class="row">
+                <div class="col-md-12">
+                        <h2>Formations</h2>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="content-item">
+                        <small>Septembre 2018 - En cours</small>
+                        <h3>Bachelor en Développement Mobile, Web
+                            et Logiciel</h3>
+                        <h4>ECE Tech</h4>
+
+                        <p>Paris, France</p>
+                        <ul>
+                            <small>
+                                <li>Android, Java et IOS</li>
+                            </small>
+                            <small>
+                                <li>HTML/CSS, AngularJS, Symfony & Bootstrap</li>
+                            </small>
+                            <small>
+                                <li>Projets Android et Web</li>
+                            </small>
+                        </ul>
+                    </div>
+
+                    <div class="content-item">
+                        <small>Septembre 2016 - Juillet 2017</small>
+                        <h3>Prépa API</h3>
+                        <h4>EPITA</h4>
+
+                        <p>Villejuif, France</p>
+                        <p>Classe préparatoire au cycle Ingénieur rassemblant les
+                            deux années de prépa intégrée en une, dans le même
+                            principe que la prépa ATS</p>
+                        <ul>
+                            <small>
+                                <li>Projet en C</li>
+                            </small>
+                            <small>
+                                <li>Mathématiques, Physique et Algorithmie</li>
+                            </small>
+                        </ul>
+                    </div>
+
+                    <div class="content-item">
+                        <small>Septembre 2014 - Juillet 2016</small>
+                        <h3>BTS Services Informatiques aux Organisations</h3>
+                        <h4>ITIC Paris</h4>
+
+                        <p>Paris, France</p>
+                        <ul>
+                            <small>
+                                <li>HTML/CSS, PHP/MySQL</li>
+                            </small>
+                            <small>
+                                <li>Java, C# et Algorithmie</li>
+                            </small>
+                            <small>
+                                <li>Projets Web et Java</li>
+                            </small>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Section Expériences -->
+    <section class="wrapper style1" id="exper">
+        <div class="inner container-fluid">
+
+            <div class="row">
+                <div class="col-md-12">
+                        <h2>Expériences</h2>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="content-item">
+                        <small>Juillet 2016 - Septembre 2016</small>
+                        <h3>Développeur Android</h3>
+                        <h4>Things of Tomorrow</h4>
+
+                        <p>Anglet, France</p>
+                        <ul>
+                            <small>
+                                <li>Mise en place des UI Tests</li>
+                            </small>
+                            <small>
+                                <li>Créations des Interfaces Graphiques de l'application Seekios</li>
+                            </small>
+                            <small>
+                                <li>Développement des fonctionnalités de l'application Seekios</li>
+                            </small>
+                        </ul>
+                    </div>
+
+                    <div class="content-item">
+                        <small>Octobre 2015 - Juillet 2016</small>
+                        <h3>Stagiare Développeur Android</h3>
+                        <h4>Things of Tomorrow</h4>
+
+                        <p>Anglet, France</p>
+                        <ul>
+                            <small>
+                                <li>Mise en place des UI Tests</li>
+                            </small>
+                            <small>
+                                <li>Créations des Interfaces Graphiques de l'application Seekios</li>
+                            </small>
+                            <small>
+                                <li>Développement des fonctionnalités de l'application Seekios</li>
+                            </small>
+                        </ul>
+                    </div>
+
+                    <div class="content-item">
+                        <small>Juin 2015 - Juillet 2015</small>
+                        <h3>Développeur Web</h3>
+                        <h4>S2ISOFT</h4>
+
+                        <p>Boissy St Léger, France</p>
+                        <p>Anglet, France</p>
+                        <ul>
+                            <small>
+                                <li>Refonte d'un site E-Commerce sous Prestashop</li>
+                            </small>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<!-- Footer -->
+<footer id="footer">
+</footer>
+
+</body>
+</html>
